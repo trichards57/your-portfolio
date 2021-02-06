d8a3078a35b6cc1d417e3f812684eed05429575c
Tony Richards <trichards57@outlook.com>
2021-02-06 11:45:53 +0000

    Starting to develop the landing page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,23 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./landing";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Landing />
-        </Route>
-      </Switch>
-    </Router>
+    <Auth0Provider
+      domain="tr-toolbox.eu.auth0.com"
+      clientId="BaYjw4rJJQONsdFN7Vxd6Km9Z38rFUKR"
+      redirectUri={window.location.origin}
+    >
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
+    </Auth0Provider>
   );
 }
 
--- client/src/landing/index.tsx
+++ client/src/landing/index.tsx
@@ -1,24 +1,54 @@
-import logo from "./logo.svg";
-import "./landing.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  Button,
+  Container,
+  makeStyles,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import { log } from "console";
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function Landing() {
+  const classes = useStyles();
+  const { loginWithRedirect } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="sm">
+      <Paper className={classes.paper} elevation={0}>
+        <Typography component="h1" variant="h5">
+          Your Portfolio
+        </Typography>
+        <Typography component="p" variant="body1">
+          Track your portfolio as you work.
+        </Typography>
+        <Button onClick={() => loginWithRedirect({ })}>
+          Register
+        </Button>
+        <Button onClick={() => loginWithRedirect()}>Login</Button>
+      </Paper>
+    </Container>
   );
 }
 
