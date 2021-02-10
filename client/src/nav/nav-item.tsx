be12a39c056f3840017636c59c028867b0cd2646
Tony Richards <trichards57@outlook.com>
2021-02-10 09:04:06 +0000

    Fixed nav links.

--- client/src/nav/nav-item.tsx
+++ client/src/nav/nav-item.tsx
@@ -1,11 +1,12 @@
 import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavItem(props: { name: string; href: string; icon: React.ReactNode }) {
   const { icon, href, name } = props;
 
   return (
-    <ListItem button component="a" href={href}>
+    <ListItem button component={Link} to={href}>
       <ListItemIcon>{icon}</ListItemIcon>
       <ListItemText primary={name} />
     </ListItem>
