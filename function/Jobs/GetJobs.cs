3dfa1ae0791d7fd8d97a159127672f1cf4b97572
Tony Richards <trichards57@outlook.com>
2021-03-04 07:36:28 +0000

    Added GetJobs endpoint.

---
+++ function/Jobs/GetJobs.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using PortfolioServer.Authentication;
+using PortfolioServer.ResponseModel;
+using PortfolioServer.Services;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioServer.Jobs
+{
+    public class GetJobs
+    {
+        private readonly IAuthenticationHelper _authenticationHelper;
+        private readonly IShiftService _shiftService;
+
+        public GetJobs(IShiftService shiftService, IAuthenticationHelper authenticationHelper)
+        {
+            _shiftService = shiftService;
+            _authenticationHelper = authenticationHelper;
+        }
+
+        [FunctionName("GetJobs")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Received shift job request.");
+
+            var claims = await _authenticationHelper.DecodeToken(req.Headers["Authorization"]);
+
+            if (claims == null || !claims.Identity.IsAuthenticated)
+            {
+                log.LogInformation("Unauthorised request received.");
+                return new UnauthorizedResult();
+            }
+
+            var query = req.Query;
+
+            if (!query.Keys.OfType<string>().Contains("shiftId"))
+                return new BadRequestResult();
+
+            var shiftId = query["shiftId"];
+
+            var shift = await _shiftService.GetShift(claims.Identity.Name, shiftId);
+
+            if (shift == null)
+                return new NotFoundResult();
+
+            return new OkObjectResult(shift.Jobs.Select(j => new JobSummary
+            {
+                Age = j.Age,
+                Category = j.Category,
+                Gender = j.Gender,
+                ReflectionFlag = j.ReflectionFlag
+            }));
+        }
+    }
+}
---
+++ function/ResponseModel/JobSummary.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using PortfolioServer.Model;
+
+namespace PortfolioServer.ResponseModel
+{
+    public class JobSummary
+    {
+        [JsonProperty("age")]
+        public int? Age { get; set; }
+
+        [JsonProperty("category")]
+        public int Category { get; set; }
+
+        [JsonProperty("gender")]
+        public Gender? Gender { get; set; }
+
+        [JsonProperty("reflectionFlag")]
+        public bool ReflectionFlag { get; set; }
+    }
+}
