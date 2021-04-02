ed862ae1b8d815e53754be2fb0d75d0b55f29c10
Tony Richards <trichards57@outlook.com>
2021-04-02 13:58:09 +0100

    Adds tests for RecentShifts.

---
+++ PortfolioServer.Test/Shifts/RecentShiftsTests.cs
@@ -0,0 +1,83 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using PortfolioServer.Model;
+using PortfolioServer.ResponseModel;
+using PortfolioServer.Services;
+using PortfolioServer.Shifts;
+using PortfolioServer.Test.Helpers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PortfolioServer.Test.Shifts
+{
+    public class RecentShiftsTests
+    {
+        [Fact]
+        public async Task ReturnsSixShifts()
+        {
+            var fixture = new Fixture();
+            var testShifts = fixture.CreateMany<Shift>(20);
+            var expectedShifts = testShifts.Where(s => s.Date.Date <= DateTime.Today).OrderByDescending(s => s.Date).Take(6).Select(s => new ShiftSummary
+            {
+                CrewMate = s.CrewMate,
+                Date = s.Date,
+                Duration = s.Duration,
+                Event = s.Event,
+                Id = s.Id,
+                Location = s.Location,
+                LoggedCalls = s.Jobs?.Count ?? 0,
+                Role = s.Role
+            });
+
+            var shiftServiceMock = new Mock<IShiftService>(MockBehavior.Strict);
+            shiftServiceMock.Setup(s => s.GetAllShifts(AuthenticationHelperMock.GoodUserId)).ReturnsAsync(testShifts);
+            var shiftService = shiftServiceMock.Object;
+
+            var function = new RecentShifts(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.GoodHeader);
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<OkObjectResult>()
+                .Which.Value.Should().BeEquivalentTo(expectedShifts);
+        }
+
+        [Fact]
+        public async Task ReturnsUnauthorisedWithBadClaims()
+        {
+            var shiftService = new Mock<IShiftService>(MockBehavior.Strict).Object;
+
+            var function = new RecentShifts(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.BadHeader);
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<UnauthorizedResult>();
+        }
+
+        [Fact]
+        public async Task ReturnsUnauthorisedWithNoClaims()
+        {
+            var shiftService = new Mock<IShiftService>(MockBehavior.Strict).Object;
+
+            var function = new RecentShifts(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<UnauthorizedResult>();
+        }
+    }
+}
