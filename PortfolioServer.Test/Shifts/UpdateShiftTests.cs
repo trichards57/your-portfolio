d7a19e353a5134f0d118c8659bd337f4d9412683
Tony Richards <trichards57@outlook.com>
2021-04-02 14:09:42 +0100

    Added test for UpdateShift.

---
+++ PortfolioServer.Test/Shifts/UpdateShiftTests.cs
@@ -0,0 +1,246 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Newtonsoft.Json;
+using PortfolioServer.Model;
+using PortfolioServer.RequestModel;
+using PortfolioServer.Services;
+using PortfolioServer.Shifts;
+using PortfolioServer.Test.Helpers;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PortfolioServer.Test.Shifts
+{
+    public class UpdateShiftTests
+    {
+        [Fact]
+        public async Task ReturnsBadRequestWithInvalidDuration()
+        {
+            var fixture = new Fixture();
+
+            var testShift = fixture.Build<NewShift>()
+                .With(j => j.Duration, TimeSpan.FromHours(-2))
+                .Create();
+
+            var body = JsonConvert.SerializeObject(testShift);
+            var bodyArray = Encoding.UTF8.GetBytes(body);
+            var bodyStream = new MemoryStream(bodyArray);
+
+            var shiftService = new Mock<IShiftService>(MockBehavior.Strict).Object;
+
+            var function = new UpdateShift(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.GoodHeader);
+            request.Body = bodyStream;
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<BadRequestResult>();
+        }
+
+        [Fact]
+        public async Task ReturnsBadRequestWithInvalidEvent()
+        {
+            var fixture = new Fixture();
+
+            var testShift = fixture.Build<NewShift>()
+                .With(j => j.Duration, TimeSpan.FromHours(2))
+                .With(j => j.Event, string.Empty)
+                .Create();
+
+            var body = JsonConvert.SerializeObject(testShift);
+            var bodyArray = Encoding.UTF8.GetBytes(body);
+            var bodyStream = new MemoryStream(bodyArray);
+
+            var shiftService = new Mock<IShiftService>(MockBehavior.Strict).Object;
+
+            var function = new UpdateShift(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.GoodHeader);
+            request.Body = bodyStream;
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<BadRequestResult>();
+        }
+
+        [Fact]
+        public async Task ReturnsBadRequestWithInvalidJson()
+        {
+            var body = JsonConvert.SerializeObject("{ invalidjson }");
+            var bodyArray = Encoding.UTF8.GetBytes(body);
+            var bodyStream = new MemoryStream(bodyArray);
+
+            var shiftService = new Mock<IShiftService>(MockBehavior.Strict).Object;
+
+            var function = new UpdateShift(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.GoodHeader);
+            request.Body = bodyStream;
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<BadRequestResult>();
+        }
+
+        [Fact]
+        public async Task ReturnsBadRequestWithInvalidRole()
+        {
+            var fixture = new Fixture();
+
+            var testShift = fixture.Build<NewShift>()
+                .With(j => j.Duration, TimeSpan.FromHours(2))
+                .With(j => j.Role, (RoleType)10)
+                .Create();
+
+            var body = JsonConvert.SerializeObject(testShift);
+            var bodyArray = Encoding.UTF8.GetBytes(body);
+            var bodyStream = new MemoryStream(bodyArray);
+
+            var shiftService = new Mock<IShiftService>(MockBehavior.Strict).Object;
+
+            var function = new UpdateShift(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.GoodHeader);
+            request.Body = bodyStream;
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<BadRequestResult>();
+        }
+
+        [Fact]
+        public async Task ReturnsNotFoundIfInvalidId()
+        {
+            var fixture = new Fixture();
+
+            var testId = fixture.Create<string>();
+            var testOriginalShift = fixture.Build<Shift>()
+                .With(j => j.Duration, TimeSpan.FromHours(2))
+                .Create();
+            var testUpdatedShift = fixture.Build<UpdatedShift>()
+                .With(j => j.Id, testOriginalShift.Id)
+                .With(j => j.Duration, TimeSpan.FromHours(3))
+                .Create();
+            var updatedShift = new Shift
+            {
+                CrewMate = testUpdatedShift.CrewMate,
+                Date = testUpdatedShift.Date,
+                Duration = testUpdatedShift.Duration,
+                Event = testUpdatedShift.Event,
+                Id = testUpdatedShift.Id,
+                Location = testUpdatedShift.Location,
+                Role = testUpdatedShift.Role,
+                UserId = testOriginalShift.UserId,
+                Jobs = testOriginalShift.Jobs
+            };
+
+            var body = JsonConvert.SerializeObject(testUpdatedShift);
+            var bodyArray = Encoding.UTF8.GetBytes(body);
+            var bodyStream = new MemoryStream(bodyArray);
+
+            var shiftServiceMock = new Mock<IShiftService>(MockBehavior.Strict);
+            shiftServiceMock.Setup(s => s.GetShift(AuthenticationHelperMock.GoodUserId, testOriginalShift.Id)).Returns(Task.FromResult<Shift>(null));
+            var shiftService = shiftServiceMock.Object;
+
+            var function = new UpdateShift(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.GoodHeader);
+            request.Body = bodyStream;
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task ReturnsOkIfValidRequest()
+        {
+            var fixture = new Fixture();
+
+            var testId = fixture.Create<string>();
+            var testOriginalShift = fixture.Build<Shift>()
+                .With(j => j.Duration, TimeSpan.FromHours(2))
+                .Create();
+            var testUpdatedShift = fixture.Build<UpdatedShift>()
+                .With(j => j.Id, testOriginalShift.Id)
+                .With(j => j.Duration, TimeSpan.FromHours(3))
+                .Create();
+            var updatedShift = new Shift
+            {
+                CrewMate = testUpdatedShift.CrewMate,
+                Date = testUpdatedShift.Date,
+                Duration = testUpdatedShift.Duration,
+                Event = testUpdatedShift.Event,
+                Id = testUpdatedShift.Id,
+                Location = testUpdatedShift.Location,
+                Role = testUpdatedShift.Role,
+                UserId = testOriginalShift.UserId,
+                Jobs = testOriginalShift.Jobs
+            };
+
+            var body = JsonConvert.SerializeObject(testUpdatedShift);
+            var bodyArray = Encoding.UTF8.GetBytes(body);
+            var bodyStream = new MemoryStream(bodyArray);
+
+            var shiftServiceMock = new Mock<IShiftService>(MockBehavior.Strict);
+            shiftServiceMock.Setup(s => s.GetShift(AuthenticationHelperMock.GoodUserId, testOriginalShift.Id)).ReturnsAsync(testOriginalShift);
+            shiftServiceMock.Setup(s => s.UpdateShift(AuthenticationHelperMock.GoodUserId, It.Is<Shift>(u => updatedShift.Equals(u)))).Returns(Task.CompletedTask);
+            var shiftService = shiftServiceMock.Object;
+
+            var function = new UpdateShift(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.GoodHeader);
+            request.Body = bodyStream;
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<OkResult>();
+
+            shiftServiceMock.Verify(s => s.UpdateShift(AuthenticationHelperMock.GoodUserId, It.Is<Shift>(u => updatedShift.Equals(u))));
+        }
+
+        [Fact]
+        public async Task ReturnsUnauthorisedWithBadClaims()
+        {
+            var shiftService = new Mock<IShiftService>(MockBehavior.Strict).Object;
+
+            var function = new UpdateShift(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.Headers.Add("Authorization", AuthenticationHelperMock.BadHeader);
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<UnauthorizedResult>();
+        }
+
+        [Fact]
+        public async Task ReturnsUnauthorisedWithNoClaims()
+        {
+            var shiftService = new Mock<IShiftService>(MockBehavior.Strict).Object;
+
+            var function = new UpdateShift(shiftService, AuthenticationHelperMock.GetAuthenticationHelper());
+
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+
+            var result = await function.Run(request, NullLogger.Instance);
+
+            result.Should().BeOfType<UnauthorizedResult>();
+        }
+    }
+}
--- function/Model/Shift.cs
+++ function/Model/Shift.cs
@@ -34,6 +34,15 @@ namespace PortfolioServer.Model
         [JsonProperty("userId")]
         public string UserId { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Shift shift))
+                return false;
+
+            return CrewMate == shift.CrewMate && Date == shift.Date && Duration == shift.Duration
+                && Event == shift.Event && Location == shift.Location && Role == shift.Role && UserId == shift.UserId;
+        }
+
         public bool ShouldSerializeJobs()
         {
             return Jobs?.Count > 0;
